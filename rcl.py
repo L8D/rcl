0a435852b57e9beeb6bcbdbfde1eaa462b78bd78
Tenor Biel <seed@BrandysMacBookPro.local>
2013-02-16 16:30:01 -0600

    Finished up the python version

--- rcl.py
+++ rcl.py
@@ -1,8 +1,19 @@
 #! /usr/bin/env python2
+import os
 import sys
 import md5
-dbfile = open('database')
-print(dbfile.read())
-print(md5.md5(dbfile.read()).hexdigest())
-filehash = md5.md5(open(sys.argv[1]).read()).hexdigest()
-print(filehash)
+def indexfile(dbfile, infile):
+  filehash = md5.md5(infile).hexdigest()
+  print(filehash)
+  hashes = dbfile.read().split("\n")
+  if filehash in hashes:
+    print("File already indexed")
+  else:
+    dbfile.write(filehash + "\n")
+if len(sys.argv) > 1:
+  if os.path.isfile(sys.argv[1]):
+    indexfile(open('database', 'r+a'), open(sys.argv[1]).read())
+  elif sys.argv[1] == "--clear" or sys.argv[1] == "-c":
+    open('database', 'w').write("")
+  else:
+    print("File not found: " + sys.argv[1])
